56ccaeb015ddfd94970c3d180317746750da03b3
gityuuhub <93773459+gityuuhub@users.noreply.github.com>
2021-12-18 23:08:58 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+console.log("123");
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox222</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
